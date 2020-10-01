76abed7322864a10e0e8a93fcd674d8f898a2429
MohamedGamalElSherbiny <mr80_thewizard@me.com>
2020-10-01 10:46:21 +0200

    Get the hardness selected title

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,15 +1,14 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]  // Dictionary of the timers for boiling eggs
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {  // Define the UIButtons in the MainStoryBoard
+        if let hardnessSelected = sender.currentTitle {  // Get user input button title
+            if let timer = eggTimes[hardnessSelected]{  // Get the time from the dictionary
+                print(timer)
+            }
+        }
+    }
 }
