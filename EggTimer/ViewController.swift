e86b2ed94e51954b9cea0670adba5e6dace56739
MohamedGamalElSherbiny <mr80_thewizard@me.com>
2020-10-01 11:20:04 +0200

    Add a countdown timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,7 +7,15 @@ class ViewController: UIViewController {
     @IBAction func hardnessSelected(_ sender: UIButton) {  // Define the UIButtons in the MainStoryBoard
         if let hardnessSelected = sender.currentTitle {  // Get user input button title
             if let timer = eggTimes[hardnessSelected]{  // Get the time from the dictionary
-                print(timer)
+                var secondsRemaining = timer * 60  // Convert minutes to seconds
+                Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in  // Create a timer with seconds and repeat every 1 second
+                    if secondsRemaining > 0 {
+                        print ("\(secondsRemaining) seconds")
+                        secondsRemaining -= 1
+                    } else {
+                        Timer.invalidate()
+                    }
+                }
             }
         }
     }
