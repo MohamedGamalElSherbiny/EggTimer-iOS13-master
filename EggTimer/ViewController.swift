6dfc141f6d751aed67d43a09253d32e589b482c6
MohamedGamalElSherbiny <mr80_thewizard@me.com>
2020-10-01 11:33:37 +0200

    Adding the progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -2,17 +2,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]  // Dictionary of the timers for boiling eggs
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 5]  // Dictionary of the timers for boiling eggs
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {  // Define the UIButtons in the MainStoryBoard
+        titleLabel.text = "How do you like your eggs?"
+        timer.invalidate()
         if let hardnessSelected = sender.currentTitle {  // Get user input button title
-            if let timer = eggTimes[hardnessSelected]{  // Get the time from the dictionary
-                var secondsRemaining = timer * 60  // Convert minutes to seconds
-                Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in  // Create a timer with seconds and repeat every 1 second
+            if let eggTimer = eggTimes[hardnessSelected]{  // Get the time from the dictionary
+                var secondsRemaining = eggTimer  // Convert minutes to seconds
+                self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in  // Create a timer with seconds and repeat every 1 second
                     if secondsRemaining > 0 {
                         print ("\(secondsRemaining) seconds")
                         secondsRemaining -= 1
                     } else {
+                        self.titleLabel.text = "Done!!"
                         Timer.invalidate()
                     }
                 }
